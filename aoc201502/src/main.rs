a48eb4fe804fd098f6c20cc3fb6dadb52d936163
Gordon Little <dev@gordonlittle.co.uk>
2026-01-18 12:40:18 +0000

    aoc201502.2

--- aoc201502/src/main.rs
+++ aoc201502/src/main.rs
@@ -7,6 +7,7 @@ fn main() {
 fn part1(){
 
     let mut total = 0;
+    let mut totalribbon: i32 = 0;
 
     let contents = fs::read_to_string("input.txt")
         .expect("Should have been able to read the file");
@@ -26,22 +27,30 @@ fn part1(){
         let mut extra = min(top, left);
         extra = min(extra, front);
 
-        let area = 2*top + 2*left + 2*front + top.min(left).min(front);
+        let area = 2*(top.0) + 2*(left.0) + 2*(front.0) + top.0.min(left.0).min(front.0);
+        let ribbon = top.1.min(left.1).min(front.1) + volume(l, w, h);
 
         println!("Area for present {}x{}x{} is {}", l, w, h, area);
+        println!("Ribbon for present {}x{}x{} is {}", l, w, h, ribbon);
         total += area;
+        totalribbon += ribbon;
     }
     println!("Total area is {}.", total);
+    println!("Total ribbon is {}.", totalribbon);
 }
 
-fn top(l: i32, w: i32) -> i32 {
-    return l * w;
+fn top(l: i32, w: i32) -> (i32, i32) {
+    return (l * w, 2 * (l + w));
 }
 
-fn left(w: i32, h: i32) -> i32 {
-    return w * h;
+fn left(w: i32, h: i32) -> (i32, i32) {
+    return (w * h, 2 * (w + h));
 }
 
-fn front(l: i32, h: i32) -> i32 {
-    return l * h;
+fn front(l: i32, h: i32) -> (i32, i32) {
+    return (l * h, 2 * (l + h));
+}
+
+fn volume(l: i32, w: i32, h: i32) -> i32 {
+    return l * w * h;
 }
