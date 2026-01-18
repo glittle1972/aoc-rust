e0b8dc9534fcfb982084e2c890e1f82da1db88fd
Gordon Little <dev@gordonlittle.co.uk>
2026-01-18 12:17:52 +0000

    aoc201501.2
    auto-complete or copilot is doing  lot of heavy lifting here. I don't know if it's been trained on past AOC problems, or has read PUZZLE.md and worked out what to do.

--- aoc201501/src/main.rs
+++ aoc201501/src/main.rs
@@ -2,13 +2,14 @@ use std::fs;
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1(){
 
-    let mut floor = 0;
+    let mut floor: i32 = 0;
 
-    let contents = fs::read_to_string("input.txt")
+    let contents: String = fs::read_to_string("input.txt")
         .expect("Should have been able to read the file");
 
     for c in contents.chars() {
@@ -20,4 +21,27 @@ fn part1(){
     }
 
     println!("Santa is on floor {floor}");
+}
+
+fn part2(){
+
+    let mut floor: i32 = 0;
+    let mut position: i32 = 0;
+
+    let contents: String = fs::read_to_string("input.txt")
+        .expect("Should have been able to read the file");
+
+    for c in contents.chars() {
+        if c == '(' {
+            floor += 1;
+        } else if c == ')' {
+            floor -= 1;
+        }
+        position += 1;
+        if floor == -1 {
+            break;
+        }
+    }
+
+    println!("Santa reaches the basement after {position} steps.");
 }
