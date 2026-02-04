258b80ef2829b6d80cf7fdd42194b633184a78a8
Gordon Little <dev@gordonlittle.co.uk>
2026-02-04 20:49:03 +0000

    aoc201506.2

--- aoc201506/src/main.rs
+++ aoc201506/src/main.rs
@@ -1,5 +1,5 @@
 use regex::Regex;
-use std::fs;
+use std::{cmp,fs};
 
 enum Op {
     On,
@@ -10,21 +10,22 @@ enum Op {
 fn main() {
     let res1 = part1("input.txt");
     println!("Result 1 is {}", res1);
+    let res2 = part2("input.txt");
+    println!("Result 2 is {}", res2);
 }
 
 fn part1(filepath: &str) -> i32 {
-    let mut lights = [false; 1000000];
+    let mut lights = vec![false; 1000000];
     let contents = fs::read_to_string(filepath)
         .expect("Could not read file");
     for l in contents.lines() {
         let (op, x1, y1, x2, y2) = parse_line(l);
-        println!("({},{}) -> ({},{})", x1, y1, x2, y2);
-        switch(&mut lights, op, x1, y1, x2, y2);
+        switch1(&mut lights, op, x1, y1, x2, y2);
     }
-    return count(&lights);
+    return count1(&lights);
 }
 
-fn count(lights: &[bool]) -> i32 {
+fn count1(lights: &[bool]) -> i32 {
     let mut result = 0;
     for b in lights.into_iter() {
         if *b {
@@ -34,7 +35,7 @@ fn count(lights: &[bool]) -> i32 {
     return result;
 }
 
-fn switch(lights: &mut[bool], op: Op, x1: usize, y1: usize, x2: usize, y2: usize) {
+fn switch1(lights: &mut[bool], op: Op, x1: usize, y1: usize, x2: usize, y2: usize) {
     for x in x1..x2+1 {
         for y in y1..y2+1 {
             lights[y * 1000 + x] = match op {
@@ -46,6 +47,36 @@ fn switch(lights: &mut[bool], op: Op, x1: usize, y1: usize, x2: usize, y2: usize
     }
 }
 
+fn part2(filepath: &str) -> i32 {
+    let mut lights = vec![0; 1000000];
+    let contents = fs::read_to_string(filepath)
+        .expect("Could not read file");
+    for l in contents.lines() {
+        let (op, x1, y1, x2, y2) = parse_line(l);
+        switch2(&mut lights, op, x1, y1, x2, y2);
+    }
+    return count2(&lights);
+}
+
+fn count2(lights: &[i32]) -> i32 {
+    let mut result = 0;
+    for v in lights.into_iter() {
+        result += v;
+    }
+    return result;
+}
+
+fn switch2(lights: &mut[i32], op: Op, x1: usize, y1: usize, x2: usize, y2: usize) {
+    for x in x1..x2+1 {
+        for y in y1..y2+1 {
+            lights[y * 1000 + x] = match op {
+                Op::On => lights[y * 1000 + x] + 1,
+                Op::Off => cmp::max(lights[y * 1000 + x] - 1, 0),
+                _ => lights[y * 1000 + x] + 2 
+            }
+        }
+    }
+}
 fn parse_line(line: &str) -> (Op, usize, usize, usize, usize) {
     let re = Regex::new(r"(?<op>turn on|toggle|turn off) (?<x1>[0-9]{1,3}),(?<y1>[0-9]{1,3}) through (?<x2>[0-9]{1,3}),(?<y2>[0-9]{1,3})").unwrap();
     let caps = re.captures(line).unwrap();
@@ -68,11 +99,24 @@ fn parse_line(line: &str) -> (Op, usize, usize, usize, usize) {
 #[cfg(test)]
 mod tests {
     use super::part1;
+    use super::part2;
 
     #[test]
     fn test1() {
-        let res = part1("test.txt");
+        let res = part1("test1.txt");
         assert_eq!(998996, res);
+    }
+
+    #[test]
+    fn test2() {
+        let res = part2("test2.txt");
+        assert_eq!(1, res);
+    }
+
+    #[test]
+    fn test3() {
+        let res = part2("test3.txt");
+        assert_eq!(2000000, res);
     }
 
 }
