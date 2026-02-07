06c5c923c67053240542ed95cef2220a2cecd61c
Gordon Little <dev@gordonlittle.co.uk>
2026-02-07 19:12:58 +0000

    aoc201508.2

--- aoc201508/src/main.rs
+++ aoc201508/src/main.rs
@@ -14,6 +14,8 @@ lazy_static! {
 fn main() {
     let res1 = part1("input.txt");
     println!("Result 1 is {}", res1);
+    let res2 = part2("input.txt");
+    println!("Result 2 is {}", res2);
 }
 
 fn part1(filepath: &str) -> usize {
@@ -24,6 +26,19 @@ fn part1(filepath: &str) -> usize {
     for l in contents.lines() {
         let (total, memory) = process_line(l);
         result += total - memory;
+    }
+
+    return result;
+}
+
+fn part2(filepath: &str) -> usize {
+    let contents = fs::read_to_string(filepath)
+        .expect("Could not read file");
+    
+    let mut result = 0;
+    for l in contents.lines() {
+        let (total, memory) = process_line2(l);
+        result += memory - total;
     }
 
     return result;
@@ -44,15 +59,37 @@ fn process_line(line: &str) -> (usize, usize) {
     return (total, memory);
 }
 
+fn process_line2(line: &str) -> (usize, usize) {
+    let total = line.bytes().len();
+//    let caps = re_line.captures(line).unwrap();
+    println!("total for {} is {}", line, total);
+    let s1 = re_backslash.replace_all(&line, "BBBB");
+    let s2 = re_hex.replace_all(&s1, "BBxXX");
+    let s3 = re_quote.replace_all(&s2, "BBBQ");
+    let s4 = re_open.replace_all(&s3, "QBQ");
+    let s5 = re_close.replace_all(&s4, "BQQ");
+    let memory = s5.bytes().len();
+    println!("total for {} is {}", s5, memory);
+
+    return (total, memory);
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::part1;
+    use super::part2;
 
     #[test]
     fn test1() {
         let res = part1("test.txt");
         assert_eq!(12, res);
+    }
+
+    #[test]
+    fn test2() {
+        let res = part2("test.txt");
+        assert_eq!(19, res);
     }
 
 }
