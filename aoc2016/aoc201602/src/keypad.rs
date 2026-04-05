cdfac51b6c0fccd49bc068d82db4b26779ffeef0
Gordon Little <dev@gordonlittle.co.uk>
2026-04-05 09:18:41 +0000

    aoc201602.2

--- aoc2016/aoc201602/src/keypad.rs
+++ aoc2016/aoc201602/src/keypad.rs
@@ -1,41 +1,54 @@
 use crate::direction::Direction;
 pub struct Keypad {
-    position: i8
+    layout: Vec<Vec<char>>,
+    x: usize,
+    y: usize
 }
 
 impl Keypad {
-    pub fn new() -> Keypad {
-        Keypad { position: 5}
+    pub fn new(layout: &str, x:usize, y: usize) -> Keypad {
+        let mut grid = vec![];
+        let lines = layout.lines();
+        for line in lines {
+            let mut row = vec![];
+            for c in line.chars() {
+                row.push(c);
+            }
+            grid.push(row);
+        }
+        Keypad { layout: grid, x, y }
     }
 
     pub fn move_direction(&mut self, direction: &Direction) {
         match direction {
             Direction::Up => {
-                let new = self.position - 3;
-                if new > 0 {
-                    self.position = new;
+                let ny = self.y - 1;
+                if self.layout[ny][self.x] != ' ' {
+                    self.y = ny;
                 }
             },
             Direction::Down => {
-                let new = self.position + 3;
-                if new < 10 {
-                    self.position = new;
+                let ny = self.y + 1;
+                if self.layout[ny][self.x] != ' ' {
+                    self.y = ny;
                 }
             },
             Direction::Right => {
-                if self.position % 3 != 0 {
-                    self.position += 1;
+                let nx = self.x + 1;
+                if self.layout[self.y][nx] != ' ' {
+                    self.x = nx;
                 }
             },
             Direction::Left => {
-                if self.position % 3 != 1 {
-                    self.position -= 1;
+                let nx = self.x - 1;
+                if self.layout[self.y][nx] != ' ' {
+                    self.x = nx;
                 }
             }
         }
     }
 
-    pub fn get_value(&self) -> i8 {
-        self.position
+    pub fn get_value(&self) -> char {
+        self.layout[self.y][self.x]
     }
 }
--- aoc2016/aoc201602/src/main.rs
+++ aoc2016/aoc201602/src/main.rs
@@ -7,8 +7,10 @@ use direction::Direction;
 use keypad::Keypad;
 
 fn main() {
-    let result = part1("input.txt");
+    let result = part1("     \n 123 \n 456 \n 789 \n     ", 2, 2, "input.txt");
     println!("Result is {result:?}");
+    let result2= part1("       \n   1   \n  234  \n 56789 \n  ABC  \n   D   \n       ", 1, 3, "input.txt");
+    println!("Result is {result2:?}");
 }
 
 fn parse_lines(contents: String, lines: &mut Vec<Vec<Direction>>) {
@@ -27,15 +29,15 @@ fn parse_lines(contents: String, lines: &mut Vec<Vec<Direction>>) {
     }
 }
 
-fn process_line(keypad: &mut Keypad, line: &Vec<Direction>) -> i8 {
+fn process_line(keypad: &mut Keypad, line: &Vec<Direction>) -> char {
     for direction in line {
         keypad.move_direction(direction);
     }
     keypad.get_value()
 }
 
-fn part1(filepath: &str) -> Vec<i8> {
-    let mut keypad = Keypad::new();
+fn part1(layout: &str, x: usize, y: usize, filepath: &str) -> Vec<char> {
+    let mut keypad = Keypad::new(layout, x, y);
     
     let contents = fs::read_to_string(filepath)
         .expect("Could not read file");
@@ -59,8 +61,15 @@ mod tests {
 
     #[test]
     fn test1() {
-        let expected: Vec<i8> = vec![1, 9, 8, 5];
-        let result= part1("test.txt");
+        let expected: Vec<char> = vec!['1', '9', '8', '5'];
+        let result= part1("     \n 123 \n 456 \n 789 \n     ", 2, 2, "test.txt");
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test2() {
+        let expected: Vec<char> = vec!['5', 'D', 'B', '3'];
+        let result= part1("       \n   1   \n  234  \n 56789 \n  ABC  \n   D   \n       ", 1, 3, "test.txt");
         assert_eq!(expected, result);
     }
 
